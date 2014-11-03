dc445d2a9b2d2908f19fe6bd6127163909b45e10
James Sun <james@getbeyond.me>
2014-11-02 17:55:10 -0800

    added break

---
+++ Caramel/DeepBreathsViewController.swift
@@ -0,0 +1,35 @@
+//
+//  DeepBreathsViewController.swift
+//  Caramel
+//
+//  Created by James Sun on 2014-11-02.
+//  Copyright (c) 2014 Beyond. All rights reserved.
+//
+
+import UIKit
+
+class DeepBreathsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationController?.navigationBar.titleTextAttributes = [ NSFontAttributeName: UIFont(name: "Univers-Light-Bold", size: 18)!]
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
