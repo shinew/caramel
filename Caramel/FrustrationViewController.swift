4260c22f555e2c019ab00c8db28b45c63b39e827
James Sun <james.sun.js@gmail.com>
2014-11-09 12:02:38 -0800

    step1-3 animation

--- Caramel/FrustrationViewController.swift
+++ Caramel/FrustrationViewController.swift
@@ -20,8 +20,29 @@ class FrustrationViewController: UIViewController {
     @IBOutlet weak var step4Button: UIButton!
     
     @IBAction func step1ButtonDidPress(sender: AnyObject) {
+        checkMarkImage1.hidden = false
+        step1Button.backgroundColor = UIColor.whiteColor()
+        step1Button.setTitleColor(UIColor.blackColor(), forState: UIControlState.Normal)
+        step2Button.backgroundColor = Conversion.UIColorFromRGB(31,green:150,blue:137)
+        step2Button.setTitleColor(UIColor.whiteColor(),forState: UIControlState.Normal)
     }
-   
+    
+    @IBAction func step2ButtonDidPress(sender: AnyObject) {
+        checkMarkImage2.hidden = false
+        step2Button.backgroundColor = UIColor.whiteColor()
+        step2Button.setTitleColor(UIColor.blackColor(), forState: UIControlState.Normal)
+        step3Button.backgroundColor = Conversion.UIColorFromRGB(31,green:150,blue:137)
+        step3Button.setTitleColor(UIColor.whiteColor(),forState: UIControlState.Normal)
+    }
+    
+    @IBAction func step3ButtonDidPress(sender: AnyObject) {
+        checkMarkImage3.hidden = false
+        step3Button.backgroundColor = UIColor.whiteColor()
+        step3Button.setTitleColor(UIColor.blackColor(), forState: UIControlState.Normal)
+        step4Button.backgroundColor = Conversion.UIColorFromRGB(31,green:150,blue:137)
+        step4Button.setTitleColor(UIColor.whiteColor(), forState: UIControlState.Normal)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
