6570600372687eba12d94a044307d5301128302e
Shine Wang <shine@getbeyond.me>
2014-11-21 23:46:26 -0800

    Font fix for when heart rate resumes

--- Caramel/DashboardCallback.swift
+++ Caramel/DashboardCallback.swift
@@ -22,6 +22,7 @@ class DashboardCallback {
         println("Received new heart rate data")
         var hrSample = HRDecoder.dataToHRSample(data)
         if hrSample != nil && hrSample!.hr != nil && hrSample!.hr < 150 {
+            self.currentHRLabel.font = UIFont(name: "Univers Light Condensed", size: 50)
             Timer.setLastHRBluetoothReceivedDate(NSDate())
             dispatch_async(dispatch_get_main_queue(), {
                 self.currentHRLabel.text = "\(hrSample!.hr!)"
