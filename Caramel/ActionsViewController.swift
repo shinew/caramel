9ce4f8e86647992a3fe9eacf8c6d65d74952f178
James Sun <james.sun.js@gmail.com>
2014-11-23 00:24:03 -0800

    changed action screen & breathing indicator

--- Caramel/ActionsViewController.swift
+++ Caramel/ActionsViewController.swift
@@ -10,11 +10,21 @@ import UIKit
 
 class ActionsViewController: UIViewController {
 
+    @IBOutlet weak var breathButton: UIButton!
+    @IBOutlet weak var breakButton: UIButton!
+    @IBOutlet weak var moreButton: UIButton!
+    var buttons = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         self.navigationController?.navigationBar.titleTextAttributes = [ NSFontAttributeName: UIFont(name: "Univers-Light-Bold", size: 18)!]
-    }
+        
+        self.breakButton.layer.borderColor = UIColor.whiteColor().CGColor
+        self.breakButton.layer.borderWidth = 0.3
 
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
--- Caramel/DeepBreathsViewController.swift
+++ Caramel/DeepBreathsViewController.swift
@@ -13,9 +13,11 @@ class DeepBreathsViewController: UIViewController {
 
     @IBOutlet weak var bubbleView: UIView!
     @IBOutlet weak var bubbleLabel: UILabel!
+    @IBOutlet weak var breathLabel: UILabel!
     
     var bubbleCounter = 1
     var shrinkingDelay = 0.0
+    var breathOutCounter = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -33,6 +35,13 @@ class DeepBreathsViewController: UIViewController {
             delay: self.shrinkingDelay,
             options: nil,
             animations: {
+                if self.breathOutCounter % 2 == 0 {
+                    self.breathLabel.text = "Breath Out"
+                }
+                else {
+                    self.breathLabel.text = "Breath In"
+                }
+                self.breathOutCounter += 1
                 self.bubbleView.transform = CGAffineTransformMakeScale(0.5, 0.5)
                 self.vibratePhone()
             }, completion: {
@@ -41,6 +50,13 @@ class DeepBreathsViewController: UIViewController {
                     delay: 1,
                     options: .AllowUserInteraction,
                     animations: {
+                        if self.breathOutCounter % 2 == 0 {
+                            self.breathLabel.text = "Breath Out"
+                        }
+                        else {
+                            self.breathLabel.text = "Breath In"
+                        }
+                        self.breathOutCounter += 1
                         self.bubbleView.transform = CGAffineTransformMakeScale(1.0, 1.0)
                         self.vibratePhone()
                     }, completion: {finished in
@@ -49,6 +65,12 @@ class DeepBreathsViewController: UIViewController {
                             self.bubbleCounter++
                             self.bubbleLabel.text = String(self.bubbleCounter)
                             self.bubbleCallback()
+                        }
+                        else {
+                            UIView.animateWithDuration(3, animations: {
+                                self.bubbleView.transform = CGAffineTransformMakeScale(0.5,0.5)
+                                self.breathLabel.text = "Great! You are now focused."
+                            })
                         }
                     }
                 )
