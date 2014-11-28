8dc844203266a93f40ef5e92ded8eb78173dd129
Shine Wang <shine@getbeyond.me>
2014-11-28 10:41:40 -0800

    Renamed Caramel to GetBeyond

--- CaramelTests/GetBeyondTests.swift
+++ CaramelTests/GetBeyondTests.swift
@@ -9,7 +9,7 @@
 import UIKit
 import XCTest
 
-class CaramelTests: XCTestCase {
+class GetBeyondTests: XCTestCase {
     
     override func setUp() {
         super.setUp()
--- CaramelTests/QueueTest.swift
+++ CaramelTests/QueueTest.swift
@@ -7,7 +7,7 @@
 //
 
 import UIKit
-import Caramel
+import GetBeyond
 import XCTest
 
 class QueueTests: XCTestCase {
